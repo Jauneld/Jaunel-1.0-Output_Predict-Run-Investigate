1ad8a86696e6d8c014068630caaa72e437722078
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-15 13:29:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#Jaunel Deamns
+# September 14, 2023
+# We will predict what  the output will be based of the information we recive. 
+# Example code
+
+print("Hello World!")
+print("I love Computing")
+print("I love Comping")
+print("I love Computing") 
+
+# My Prediction - Add your prediction as a comment below.
+  ##I think it will print Hello World
+   ###My prediction was correct
+ 
+
+
+##### Task Investigate
+
+# Which part of the example code is the output statement?
+ ## Answer: The output statment is print()
+
+# Which part of the example code is the string?
+ ## Answer: The string is "Hello World!"
+
+# What would the output of the code print("I love Computing") be?
+ ## Answer: The output wil be I love Computing
+
+# What would happen if the code print("I love Comping") was run?
+ ## Answer: The console will show I love Comping
+
+# What would happen if the code print("I love Computing" was run?
+ ## Answer: There will be an error because there is no parenthesis. 
